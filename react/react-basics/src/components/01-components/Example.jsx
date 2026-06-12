fb0a7f50e4af9139bf98aa17237c902fb53841dc
nasubi64 <nasumonagamori@yahoo.co.jp>
2026-06-12 14:13:59 +0900

    Add React CSS example and clean up Vite default styles

--- react/react-basics/src/components/01-components/Example.jsx
+++ react/react-basics/src/components/01-components/Example.jsx
@@ -1,10 +1,21 @@
-function Example() {
+import "./Example.css";
+
+// function Example() {
+//     return (
+//         <div>
+//             <h1>Hello, World!</h1>
+//             <p>これはコンポーネントの例です。</p>
+//         </div>
+//     );
+// }
+
+const Example = () => {
     return (
-        <div>
+        <div className="component">
             <h1>Hello, World!</h1>
             <p>これはコンポーネントの例です。</p>
         </div>
     );
-}
+};
 
 export default Example;
